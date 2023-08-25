a5638cbb2ac103a70d5f4a375baa61fef5e7f2b8
ksooh <71632157+ksooh@users.noreply.github.com>
2023-08-25 14:45:04 +0900

    나의 위치 마커 업데이트

--- main.js
+++ main.js
@@ -745,7 +745,7 @@ if (navigator.geolocation) {
             message = '<div style="padding:5px;margin-left:25px;">나의 위치</div>'; // 인포윈도우에 표시될 내용입니다
         
         // 마커와 인포윈도우를 표시합니다
-        displayMarker(locPosition, message);
+        displayMarker2(locPosition, message);
       
             
       });
@@ -776,12 +776,19 @@ if (navigator.geolocation) {
 
 
 // 지도에 마커와 인포윈도우를 표시하는 함수입니다
-function displayMarker(locPosition, message) {
+function displayMarker2(locPosition, message) {
+  var imageSrc = 'https://i.ibb.co/QQtBkXB/image.png', // 마커이미지의 주소입니다   
+      imageSize = new kakao.maps.Size(32, 50), // 마커이미지의 크기입니다
+      imageOption = {offset: new kakao.maps.Point(27, 69)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+   var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize)
 
+  
+  
     // 마커를 생성합니다
     var marker = new kakao.maps.Marker({  
         map: map, 
-        position: locPosition
+        position: locPosition,
+        image:markerImage
     }); 
     
     var iwContent = message, // 인포윈도우에 표시할 내용
@@ -799,4 +806,3 @@ function displayMarker(locPosition, message) {
     // 지도 중심좌표를 접속위치로 변경합니다
     map.setCenter(locPosition);      
 }    
-
